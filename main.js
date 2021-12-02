822605125aa00a40f721491ac50efec9e114385e
AshwinCod <85323179+AshwinCod@users.noreply.github.com>
2021-12-01 17:24:55 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var speechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new speechRecognition();
+
+function Start(){
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function(event){
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+}
